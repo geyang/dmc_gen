051fefc470931810c856e89629d481b06cc8d530
Ge Yang <yangge1987@gmail.com>
2021-03-13 09:24:32 -0500

    change the make_agent to dynamic import

--- dmc_gen/algorithms/__init__.py
+++ dmc_gen/algorithms/__init__.py
@@ -1,20 +1,5 @@
-from .curl import CURL
-from .pad import PAD
-from .rad import RAD
-from .sac import SAC
-from .soda import SODA
-
-algorithm = {
-    'sac': SAC,
-    'rad': RAD,
-    'curl': CURL,
-    'pad': PAD,
-    'soda': SODA
-}
-
-
 def make_agent(algo, obs_shape, act_shape, args):
-    from .. import algorithms
+    from importlib import import_module
 
-    m = getattr(algorithms, algo.lower())
-    return getattr(m, algo.upper())(obs_shape, act_shape, args)
+    m = import_module("dmc_gen.algorithms." + algo.lower())
+    return m.__dict__[algo.upper()](obs_shape, act_shape, args)
